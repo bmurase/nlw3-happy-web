0b33e6e8b35ae26d6d1e9f59dccbaf8031855563
bmurase <biamurase@gmail.com>
2020-12-22 20:49:57 -0300

    feat: 🎸 map marker

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,12 +1,19 @@
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, Marker, TileLayer } from 'react-leaflet';
+import Leaflet from 'leaflet';
 
 import 'leaflet/dist/leaflet.css';
 
 import mapMarkerImg from '../images/map-marker.svg';
 
 import '../styles/pages/orphanages-map.css';
+
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMarkerImg,
+  iconSize: [58, 68],
+  iconAnchor: [29, 68]
+})
 
 const OrphanagesMap: React.FC = () => {
   return (
@@ -32,6 +39,11 @@ const OrphanagesMap: React.FC = () => {
       >
         {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
         <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+
+        <Marker 
+          position={[-23.4090972,-51.9474355]} 
+          icon={mapIcon}
+        />
       </MapContainer>
 
       <Link to="" className="create-orphanage">
