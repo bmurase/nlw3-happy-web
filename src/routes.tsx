668f5ac0e5ed2d41cbeadb2e24df979de55b70b8
bmurase <biamurase@gmail.com>
2020-12-24 18:22:35 -0300

    refactor: 💡 rename page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import CreateOrphanage from './pages/CreateOrphanage';
 import Landing from './pages/Landing';
-import Orphanage from './pages/Orphanage';
+import OrphanageDetail from './pages/OrphanageDetail';
 import OrphanagesMap from './pages/OrphanagesMap';
 
 const Routes: React.FC = () => {
@@ -12,7 +12,7 @@ const Routes: React.FC = () => {
         <Route path="/app" component={OrphanagesMap} />
 
         <Route path="/orphanages/create" component={CreateOrphanage} />
-        <Route path="/orphanages/:id" component={Orphanage} />
+        <Route path="/orphanages/:id" component={OrphanageDetail} />
       </Switch>
     </BrowserRouter>
   );
